96e4d558dabefe7c4c06052ce3e4a1616a14bbee
Kent Are <kent@nhi.no>
2022-03-23 12:29:58 +0100

    fix: working example with storybook

--- apps/storybook/.storybook/preview.js
+++ apps/storybook/.storybook/preview.js
@@ -1,10 +1,10 @@
 // This does not work
 // neither does relative path
 // but it does work to add css classes to globals.css
-import "ui/globals.css"
+// import "ui/globals.css"
 
 // This works
-// import "../styles/globals.css"
+import "../styles/globals.css"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
